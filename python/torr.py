b0314ef2b8c7c044cd498c1e2d9f24cb6e0854ae
Alex Vyber <alexvyber@gmail.com>
2022-07-03 06:04:00 +0300

    some more python

--- python/torr.py
+++ python/torr.py
@@ -4,16 +4,16 @@ import time
 from selenium.webdriver.common.by import By
 
 # Hard-coded url
-url = ""
+url = "https://torrentgalaxy.mx/torrents.php?c12=1&c14=1&search=devops&lang=0&nox=2#results"
 
 driver = webdriver.Chrome()
 
 # Range is used to generate urls for pages
-for n in range(2, 8):
-    req_url=url+ str(n-1)
+for n in range(3, 4):
+    req_url=url  + str(n)
 
     driver.get(req_url)
-    time.sleep(20)
+    time.sleep(5)
 
     links = driver.find_elements(By.XPATH, '//a[@role="button"]')
     # Remove first link, because it's irrelevant
